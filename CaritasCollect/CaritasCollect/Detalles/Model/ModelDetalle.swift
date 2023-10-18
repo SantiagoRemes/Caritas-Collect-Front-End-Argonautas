47e8434f941888f9711e080b9cad3e66e8778d65
SantiagoRemes <110937800+SantiagoRemes@users.noreply.github.com>
2023-10-17 23:12:57 -0700

    Final

--- CaritasCollect/CaritasCollect/Detalles/Model/ModelDetalle.swift
+++ CaritasCollect/CaritasCollect/Detalles/Model/ModelDetalle.swift
@@ -10,7 +10,7 @@ import Foundation
 func GetDetalles(id: Int) -> RecoleccionDetalles {
     var res : RecoleccionDetalles = RecoleccionDetalles(_id_donador: 0, _id_recibo: 0, _id_recolector: 0, comentarios: "", lat: 0, lon: 0, direccion: "", estado_recogido: "", lnombre_donador: "", pnombre_donador: "", tel_casa: 0, tel_celular: 0)
     
-    guard let url = URL(string:"http://192.168.1.131:10206/detalles?id=\(id)") else {
+    guard let url = URL(string:"https://equipo05.tc2007b.tec.mx:10206/detalles?id=\(id)") else {
         return res
     }
     
@@ -61,7 +61,7 @@ func PutEstado(id: Int, estado: String, comentarios: String){
     let jsonData = try? JSONSerialization.data(withJSONObject: body)
     
 
-    let url = URL(string: "http://192.168.1.131:10206/estado")!
+    let url = URL(string: "https://equipo05.tc2007b.tec.mx:10206/estado")!
     var request = URLRequest(url: url)
     request.httpMethod = "PUT"
     
